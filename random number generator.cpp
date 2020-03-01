6b21a0a3cd65e0083733735f95a92e6f390e9e6d
Soarov Chakraborty <sourovchakraborty420@gmail.com>
2020-03-01 09:16:31 +0600

    Update random number generator.cpp

--- random number generator.cpp
+++ random number generator.cpp
@@ -1,15 +1,15 @@
 //random number generator
 #include<bits/stdc++.h>
 using namespace std;
-typedef int ll;
+typedef long long ll;
 mt19937_64 rd;
 int main()
 {
-    uniform_int_distribution<int> rng(1, 10);
+    uniform_int_distribution<ll> temp(1, 10);
     ll t=10;
     while(t)
     {
         t--;
-        cout<<rng(rd)<<endl;
+        cout<<temp(rd)<<endl;
     }
 }
